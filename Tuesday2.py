7c44f7aa32e980b79bcfc762e4ccf3f497d3c4d6
kaymsalazar <94642910+kaymsalazar@users.noreply.github.com>
2021-11-18 11:58:41 -0800

    Add files via upload

---
+++ Tuesday2.py
@@ -0,0 +1,49 @@
+# Tuesday Nov. 16
+
+y = 6
+x = [1,2]
+
+#create list
+
+print("--->Everything in my possesion")
+
+inventory = ["car","shirts","food","apple bottom jeans"]
+
+# print variables
+
+print(y)
+print(x)
+
+print(inventory)
+print(inventory[2])
+
+# first loop
+
+print("-->Regular loop")
+
+count = 0
+
+while count < 4:
+    print(inventory[count])
+    count = count + 1
+
+# second loop
+
+print("--->Backward loop")
+
+count = 3
+
+while count < 0:
+    print(inventory[count])
+    count = count - 1
+
+# for loop
+
+print("--->The inventory for loop")
+
+for count in inventory:
+    print(count)
+
+A= 65
+print = A
+ 
